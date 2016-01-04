a353782bbf661bb72f3e3c519a1fa984f8e56baa
Lukasz Kuczborski <lukasz.kuczborski@allegrogroup.com>
2016-01-04 17:01:16 +0100

    Refactoring

--- SwiftMoyaCodeGenerator.js
+++ SwiftMoyaCodeGenerator.js
@@ -19,13 +19,13 @@ var SwiftMoyaCodeGenerator = function() {
         if (query) {
           var fragments = query.split('&');
           var keyvalue = fragments[0].split('=');
-          var queryParamsType = keyvalue[0] + ": " + (isNumber(keyvalue[1]) ? "Int" : "String");
+          var queryParamsType = keyvalue[0] + ": " + typeForObject(keyvalue[1]);
           var queryParamsTemplate = "_";
           var queryParams = "let " + keyvalue[0];
           var queryDictString = "\"" + keyvalue[0] + "\": " + keyvalue[0]
           for (var i = 1; i < fragments.length; i++) {
               keyvalue = fragments[i].split('=');
-              queryParamsType += ", " + keyvalue[0] + ": " + (isNumber(keyvalue[1]) ? "Int" : "String");
+              queryParamsType += ", " + keyvalue[0] + ": " + typeForObject(keyvalue[1]);
               queryParamsTemplate += ", _";
               queryParams += ", let " + keyvalue[0];
               queryDictString += ", \"" + keyvalue[0] + "\": " + keyvalue[0];
@@ -42,6 +42,8 @@ var SwiftMoyaCodeGenerator = function() {
 }
 
 function isNumber(obj) { return !isNaN(parseFloat(obj)) }
+
+function typeForObject(obj) { return (isNumber(obj) ? "Int" : "String") }
 
 SwiftMoyaCodeGenerator.identifier = "com.kuczborski.PawExtensions.SwiftMoyaCodeGenerator";
 SwiftMoyaCodeGenerator.title = "Swift (Moya)";

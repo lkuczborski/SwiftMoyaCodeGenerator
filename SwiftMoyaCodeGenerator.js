bab3e314e12457523606c35ec226dff9579744b3
Lukasz Kuczborski <lukasz.kuczborski@allegrogroup.com>
2016-01-04 17:56:18 +0100

    Stylistic changes

--- SwiftMoyaCodeGenerator.js
+++ SwiftMoyaCodeGenerator.js
@@ -1,42 +1,42 @@
 var uri = require('/URI');
 var mustache = require('./mustache');
+
 var SwiftMoyaCodeGenerator = function() {
 
     this.generate = function(context) {
 
         var template = readFile("moya.mustache");
-
         var request = context.getCurrentRequest();
         var url = uri.parse(request.url);
 
         var view = {
-          "request": request,
-          "baseURL": url.protocol + "://" + url.hostname,
-          "pathExtension": url.path,
+            "request": request,
+            "baseURL": url.protocol + "://" + url.hostname,
+            "pathExtension": url.path,
         };
 
         var query = url.query;
         if (query) {
-          var fragments = query.split('&');
-          var keyvalue = fragments[0].split('=');
-          var queryParamsType = keyvalue[0] + ": " + typeForObject(keyvalue[1]);
-          var queryParamsTemplate = "_";
-          var queryParams = "let " + keyvalue[0];
-          var queryDictString = "\"" + keyvalue[0] + "\": " + keyvalue[0]
-          for (var i = 1; i < fragments.length; i++) {
-              keyvalue = fragments[i].split('=');
-              queryParamsType += ", " + keyvalue[0] + ": " + typeForObject(keyvalue[1]);
-              queryParamsTemplate += ", _";
-              queryParams += ", let " + keyvalue[0];
-              queryDictString += ", \"" + keyvalue[0] + "\": " + keyvalue[0];
-          }
+            var fragments = query.split('&');
+            var keyvalue = fragments[0].split('=');
+            var queryParamsType = keyvalue[0] + ": " + typeForObject(keyvalue[1]);
+            var queryParamsTemplate = "_";
+            var queryParams = "let " + keyvalue[0];
+            var queryDictString = "\"" + keyvalue[0] + "\": " + keyvalue[0]
+            for (var i = 1; i < fragments.length; i++) {
+                keyvalue = fragments[i].split('=');
+                queryParamsType += ", " + keyvalue[0] + ": " + typeForObject(keyvalue[1]);
+                queryParamsTemplate += ", _";
+                queryParams += ", let " + keyvalue[0];
+                queryDictString += ", \"" + keyvalue[0] + "\": " + keyvalue[0];
+            }
 
-          view["queryParamsType"] = queryParamsType;
-          view["queryParamsTemplate"] = queryParamsTemplate;
-          view["queryParams"] = queryParams;
-          view["queryDictString"] = queryDictString;
+            view["queryParamsType"] = queryParamsType;
+            view["queryParamsTemplate"] = queryParamsTemplate;
+            view["queryParams"] = queryParams;
+            view["queryDictString"] = queryDictString;
         }
-
+        
         return mustache.render(template, view);
     }
 }
